434f48c8cbd4bc8af0f42b5ef253049246fd7757
vatiza <rakibur.mac@gmail.com>
2024-01-27 01:28:03 +0600

    show population

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
+
+
     </div>
   );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  }, [])
+
+console.log(countries);
+  return (
+    <div>
+      <h1>Visiting Every Countries</h1>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+function Country(props){
+  return(
+    <div>
+      <h2>Name:{props.name}</h2>
+      <h4>Population : {props.population}</h4>
+    </div>
+  )
 }
 
 export default App;
